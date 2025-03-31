5fcec856bb5a96b858d4e04d7e66452eacffbdef
Eduardo Martínez Cobos <edumar11@ucm.es>
2025-03-31 19:12:56 +0200

    A TOPE TODO GODIN

--- Practica2/plantilla/anyadir_resultados.py
+++ Practica2/plantilla/anyadir_resultados.py
@@ -2,58 +2,58 @@
 Modulo que permite introducir la información y los resultados de los partidos
 y los introduce en la base de datos
 """
-from typing import List
-import random
+import csv
+import os
+
 from app import db, create_app
-from app.modelos import Carta, Usuario
-from sqlalchemy import select
+from app.modelos import Partido, Historico
 
-
-def insertar_cartas_en_mazo_aleatorio(ids_cartas_disponibles: List[int], id_mazo: int,
-                                      n_cartas: int) -> None:
-    """
-    Funcion que inserta cartas aleatoriamente
-    """
-    # Seleccionamos un numero de cartas igual a n_cartas, sin repeticiones
-    id_cartas_aleatorias = random.sample(ids_cartas_disponibles, n_cartas)
-    for id_carta_aleatoria in id_cartas_aleatorias:
-        en_banquillo = random.choice([True, False])
-        numero_copias = random.randint(1, 4)
-        # Incluyo las cartas aleatoriamente
-        db.session.add(CartaEnMazo(id_carta=id_carta_aleatoria, id_mazo=id_mazo,
-                                   numero_copias=numero_copias, banquillo=en_banquillo))
-    db.session.commit()
-
-
-def generar_mazo_aleatorio(id_creador: int, nombre_mazo: str, ids_cartas_disponibles: List[int]):
-    mazo = Mazo(nombre=nombre_mazo, id_creador=id_creador)
-    db.session.add(mazo)
-    db.session.commit()
-    # Despues del commit, si accedo al id lo cargo
-    insertar_cartas_en_mazo_aleatorio(ids_cartas_disponibles, mazo.id, 40)
-
-
-def seleccionar_usuarios_mazos_aleatorios(ids_cartas_disponibles: List[int], ids_usuarios_disponibles: List[int],
-                                          n_mazos: int):
-    usuarios_con_mazo = random.choices(ids_usuarios_disponibles, k=n_mazos)
-    for i, id_usuario_aleatorio in enumerate(usuarios_con_mazo):
-        generar_mazo_aleatorio(id_usuario_aleatorio, f"mazo_{i}",
-                               ids_cartas_disponibles)
-
-
-def crear_datos_aleatorios():
-    # Creo una instancia de la aplicacion para inicializar la base de datos
+def anyadir_resultados(lista_partidos):
     flask_app = create_app()
 
-    # Para ejecutar codigo en el contexto de la aplicacion, debemos utilizar
-    # esta sentencia
     with flask_app.app_context():
+        with open(lista_partidos, newline='', encoding='utf-8') as archivo:
+            lector = csv.reader(archivo)
+            next(lector)
 
-        # Selecciono los usuarios y las cartas disponibles
-        usuarios = db.session.scalars(select(Usuario.id)).fetchall()
-        cartas = db.session.scalars(select(Carta.id)).fetchall()
-        seleccionar_usuarios_mazos_aleatorios(cartas, usuarios, 200)
+            partidos = [
+                Partido(
+                    id_partido=fila[0],
+                    fecha=fila[1],
+                    equipo_local=fila[2],
+                    equipo_visitante=fila[3],
+                    gana_local=(fila[4].strip() == "t"),
+                    url=fila[5]
+                )
+                for fila in lector
+            ]
 
+        db.session.add_all(partidos)
+        db.session.commit()
+
+def anyadir_historico(lista_historico):
+    flask_app = create_app()
+
+    with flask_app.app_context():
+        with open(lista_historico, newline='', encoding='utf-8') as archivo:
+            lector = csv.reader(archivo)
+            next(lector)
+
+            historicos = [
+                Historico(
+                    id_partido=fila[0],
+                    id_jugador=fila[1],
+                    tiempo_jugado=fila[2],
+                    puntos_marcados=fila[3],
+                    puntuacion=fila[4],
+                )
+                for fila in lector
+            ]
+
+        db.session.add_all(historicos)
+        db.session.commit()
 
 if __name__ == "__main__":
-    crear_datos_aleatorios()
+    RUTA_ACTUAL = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    anyadir_resultados(os.path.join(RUTA_ACTUAL, "partido2.csv"))
+    anyadir_historico(os.path.join(RUTA_ACTUAL, "historico2.csv"))
--- Practica2/plantilla/computar_puntuacion.py
+++ Practica2/plantilla/computar_puntuacion.py
@@ -2,12 +2,9 @@
 Modulo que permite actualizar las puntuaciones de los usuarios
 en las distintas ligas
 """
-from typing import List
-import random
 
-from Practica2.plantilla.app.rutas import cartas_usuario_en_liga
 from app import db, create_app
-from app.modelos import Carta, Usuario, Liga, Participa_liga, Carta_liga
+from app.modelos import Carta, Usuario, Participa_liga, Carta_liga
 from sqlalchemy import select, update, and_
 
 
@@ -19,16 +16,27 @@ def computar_puntuaciones():
     # esta sentencia
     with flask_app.app_context():
 
-        # Seleccionamos las cartas de cada usuario en cada liga
         usuarios = db.session.scalars(select(Usuario.id)).fetchall()
         for usuario in usuarios:
             ligas = db.session.scalars(select(Participa_liga.id_liga).where(Participa_liga.id_usuario == usuario)).fetchall()
             for liga in ligas:
-                cartas_liga = db.session.scalars(select(Carta_liga.id_jugador)
-                                           .where(and_(Carta_liga.id_liga == liga, Carta_liga.id_usuario == usuario))
-                                           .group_by(Carta_liga.id_jugador))
-                for carta_liga in cartas_liga:
-                    puntuacion_acumulada = db.session.scalars()
+
+                cartas_liga = db.session.scalars(
+                    select(Carta.puntuacion)
+                    .join(Carta_liga, Carta.id_jugador == Carta_liga.id_jugador)
+                    .where(and_(Carta_liga.id_liga == liga, Carta_liga.id_usuario == usuario))
+                    .group_by(Carta.id_jugador)
+                ).fetchall()
+
+                puntuacion_total = sum(cartas_liga) if cartas_liga else 0
+
+                db.session.execute(
+                    update(Participa_liga)
+                    .where(and_(Participa_liga.id_liga == liga, Participa_liga.id_usuario == usuario))
+                    .values(puntuacion_acumulada=puntuacion_total)
+                )
+
+            db.session.commit()
 
 
 if __name__ == "__main__":
